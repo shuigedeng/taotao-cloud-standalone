dac546e987e97b38a117f9e6a61bb7ff95d4272e
shuigedeng <981376577@qq.com>
2026-08-22 11:49:39 +0800

    开启2026.09

--- taotao-cloud-standalone-domain/src/main/java/com/taotao/cloud/standalone/domain/entity/DeptEntity.java
+++ taotao-cloud-standalone-domain/src/main/java/com/taotao/cloud/standalone/domain/entity/DeptEntity.java
@@ -55,7 +55,6 @@ public class DeptEntity extends AggregateRoot<Long> {
      * 校验名称
      *
      * @param count 数量
-     * @return 无返回值
      * @since 2022.03
      */
     public void checkName( long count ) {
@@ -67,7 +66,6 @@ public class DeptEntity extends AggregateRoot<Long> {
     /**
      * 校验
      *
-     * @return 无返回值
      * @since 2022.03
      */
     public void checkIdAndPid() {
